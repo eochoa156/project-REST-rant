96ac01dada6bd0b4b77d770f9b060a53dd387088
Emily Ochoa <eochoa156@gmail.com>
2024-03-03 08:51:40 -0800

    added 404 page

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 PAGE NOT FOUND</h1>')
 })
 
-app.listen(3000)
+app.listen(process.env.PORT);
+
+
