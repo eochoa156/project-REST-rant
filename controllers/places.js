97042bdce9f7852add5285d2c3d0865560675c44
Emily Ochoa <eochoa156@gmail.com>
2024-03-12 16:07:16 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/:id', (req, res) =>{
+router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) =>{
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id] })
+    res.render('places/show', { place: places[id], id })
   }
 })
 
@@ -36,5 +36,18 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
 
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,28 @@ function Show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <h2>Rating</h2>
+              <p>
+                Not Rated
+              </p>
+            <h2>Description</h2>
+              <p>
+              Located in { data.place.city }, 
+              { data.place.state } and the cuisines are 
+              { data.place.cuisines }
+              </p>
+            <h2>Comments</h2>
+              <p>
+                No comments yet
+              </p>
+            <a href = {`/places/${data.id}/edit`} className = "btn btn-warning">
+              Edit
+            </a>
+            <form method = "POST" action = {`/places/${data.id}?_method=DELETE`}>
+              <button type = "submit" className = "btn btn-danger">
+                Delete
+              </button>
+            </form>
           </main>
         </Def>
     )
