ccb84bef3be50694fa61c4503680debc7768af96
Emily Ochoa <eochoa156@gmail.com>
2024-03-12 08:51:14 -0700

    fixed missing index in the index.jsx file

--- controllers/places.js
+++ controllers/places.js
@@ -9,7 +9,7 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) =>{
   let id = Number(req.params.id)
   if (isNaN(id)) {
     res.render('error404')
@@ -18,7 +18,7 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id]})
+    res.render('places/show', { place: places[id] })
   }
 })
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,11 @@
 const React = require('react')
 const Def = require('../default')
 
-function Show () {
+function Show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <h1>{ data.place.name }</h1>
           </main>
         </Def>
     )
