b949e4232e350bd844d6899353409bbc6dd0a4ed
Emily Ochoa <eochoa156@gmail.com>
2024-03-12 20:52:00 -0700

    fixed error on delete part

--- controllers/places.js
+++ controllers/places.js
@@ -25,10 +25,10 @@ router.get('/:id', (req, res) => {
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
+      res.render('error404')
   }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
     res.render('places/edit', { place: places[id] })
@@ -47,6 +47,33 @@ router.post('/', (req, res) => {
   }
   places.push(req.body)
   res.redirect('/places')
+})
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
 })
 
 router.delete('/:id', (req, res) => {
--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const methodOverride = require('method-override')
 //Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use('/places', require('./controllers/places'));
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
-
+app.use('/places', require('./controllers/places'));
 
 //Controllers and Routes
 app.get('/', (req, res) => {
