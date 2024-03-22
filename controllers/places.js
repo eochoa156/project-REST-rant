bac0069668e416104f73d406057121a400571de5
Emily Ochoa <eochoa156@gmail.com>
2024-03-21 17:40:30 -0700

    comments added

--- controllers/places.js
+++ controllers/places.js
@@ -41,22 +41,27 @@ router.get('/:id/edit', (req, res) => {
 })
 
 //CREATE
-router.post('/', (req, res) => {
+router.post('/', (req, res) => { 
+  if (req.body.pic === '') { req.body.pic = undefined }
+    if (req.body.city === '') { req.body.city = undefined }
+    if (req.body.state === '') { req.body.state = undefined }
   db.Place.create(req.body)
     .then((places) => {
       res.redirect('/places')
     })
     .catch(err => {
+      let message = ''
+      console.log(err)
       if (err && err.name == 'ValidationError') {
         for (var field in err.errors) {
           message += `${field} was ${err.errors[field].value}.`
           messgae += `rs${err.errors[field].message}`
         }
         console.log('Validation error message', message)
-        res.render('places/new', { message })
+        res.render('/places/new', { message })
       }
       else {
-        res.render('error404')
+        res.render('/error404')
       }
     })
 })
@@ -64,6 +69,28 @@ router.post('/', (req, res) => {
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
 })
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
 
 //PUT update places
 router.put('/:id', (req, res) => {
--- models/places.js
+++ models/places.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: {type: String, default:'../public/images/sleepy-cat.jpg'},
+  pic: {type: String, default:'/images/sleepy-cat.jpg'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
   founded: {
     type: Number,
-    min: [ 1673, 'Sureky not that old?!?'],
+    min: [ 1673, 'Surely not that old?!?'],
     max: [new Date().getFullYear(), 'Hey this year is in the future!']
   },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
